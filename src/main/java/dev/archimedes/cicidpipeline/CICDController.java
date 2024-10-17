91967c4f6c3b32dd04348f38f33957aee30ddf87
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 12:27:41 +0530

    Initial Commit

---
+++ src/main/java/dev/archimedes/cicidpipeline/CICDController.java
@@ -0,0 +1,19 @@
+package dev.archimedes.cicidpipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CICDController {
+
+  @GetMapping("/")
+  public HelloWorld hello() {
+    return new HelloWorld("Hello, World!");
+  }
+
+  public static record HelloWorld(
+      String message
+  ) {
+
+  }
+}
---
+++ src/test/java/dev/archimedes/cicidpipeline/CicidPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.archimedes.cicidpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicidPipelineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
