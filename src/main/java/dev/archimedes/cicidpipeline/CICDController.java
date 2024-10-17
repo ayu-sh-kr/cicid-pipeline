476d5b150cbb03e86d3086ace17875958d1a0a5e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 20:37:55 +0530

    Check Changes

--- src/main/java/dev/archimedes/cicidpipeline/CICDController.java
+++ src/main/java/dev/archimedes/cicidpipeline/CICDController.java
@@ -3,12 +3,23 @@ package dev.archimedes.cicidpipeline;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CICDController {
 
   @GetMapping("/")
-  public HelloWorld hello() {
-    return new HelloWorld("Hello, World! -> Added docker on EC2");
+  public List<HelloWorld> hello() {
+    return List.of(
+        new HelloWorld("Created Continuous Integration"),
+        new HelloWorld("Created Docker File"),
+        new HelloWorld("Login to AWS ECR"),
+        new HelloWorld("Uploaded Docker Image to ECR"),
+        new HelloWorld("Created Continuous Deployment"),
+        new HelloWorld("Login to docker via AWS ECR"),
+        new HelloWorld("Pull the docker image AWS ECR"),
+        new HelloWorld("Created container and deployed on EC2")
+    );
   }
 
   public record HelloWorld(
