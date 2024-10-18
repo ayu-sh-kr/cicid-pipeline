60e83b1ded98359c52b0572dda7fdbb71d0a3220
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-18 15:28:26 +0530

    Update CICDController.java
    added new message

--- src/main/java/dev/archimedes/cicidpipeline/CICDController.java
+++ src/main/java/dev/archimedes/cicidpipeline/CICDController.java
@@ -1,5 +1,6 @@
 package dev.archimedes.cicidpipeline;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,23 +10,25 @@ import java.util.List;
 public class CICDController {
 
   @GetMapping("/")
-  public List<HelloWorld> hello() {
+  public List<Message> hello() {
     return List.of(
-        new HelloWorld("Created Continuous Integration"),
-        new HelloWorld("Created Docker File"),
-        new HelloWorld("Login to AWS ECR"),
-        new HelloWorld("Uploaded Docker Image to ECR"),
-        new HelloWorld("Created Continuous Deployment"),
-        new HelloWorld("Login to docker via AWS ECR"),
-        new HelloWorld("Pull the docker image AWS ECR"),
-        new HelloWorld("Created container and deployed on EC2"),
-        new HelloWorld("Create new EC2 instance, testing without AWS CLI"),
-        new HelloWorld("Test with new EC2 instance completed successfully")
+        new Message("Created Continuous Integration"),
+        new Message("Created Docker File"),
+        new Message("Login to AWS ECR"),
+        new Message("Uploaded Docker Image to ECR"),
+        new Message("Created Continuous Deployment"),
+        new Message("Login to docker via AWS ECR"),
+        new Message("Pull the docker image AWS ECR"),
+        new Message("Created container and deployed on EC2"),
+        new Message("Create new EC2 instance, testing without AWS CLI"),
+        new Message("Test with new EC2 instance completed successfully"),
+        new Message("Testing Automation by removing hardcoded value from CD action file")
     );
   }
 
-  public record HelloWorld(
-      String message
+  public record Message(
+      @JsonProperty("message")
+      String value
   ) {
 
   }
